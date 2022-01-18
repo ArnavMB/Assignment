9dcfbaff2056dfabe6a4d76fc762e7010fdad517
ArnavMB <97971048+ArnavMB@users.noreply.github.com>
2022-01-18 21:43:15 +0530

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,29 @@
+#list 
+list1 = [1,2,3,4,'ab',99.20,True,'a']
+list1.append('b3')
+print(list1)
+print(type(list1))
+
+#set
+tt = set()
+print(type(tt))
+
+#dictionary
+i = {'name':'arnav','age':21}
+print(f"your dictionary is {i}")
+i.keys()
+i.values()
+i['age'] =22
+type(i)
+print(f"new updated dict is {i}")
+
+#set
+a = {1,2,3,4,3,2,7,9,'a','5j'}
+print(f"Your set is {a}")
+print(type(a))
+
+#tuple
+n = ('a','b',1,3,2,12,11,3,1)
+print(n.count(1))
+print(type(n))
+print(f"your tuple is{n}")
